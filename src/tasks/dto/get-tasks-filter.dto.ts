de3f728bb84dbc17fe0c5404e2e934579a453211
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-06-17 23:39:24 +0530

    Added filter validation

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,14 @@
+import { IsIn, IsNotEmpty, IsOptional } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
+  // Ennsures its optional
+  @IsOptional()
+  // If value exist, it should be one of th following
+  @IsIn([TaskStatus.IN_PROGRESS, TaskStatus.DONE, TaskStatus.OPEN])
   status: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
   search: string;
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -26,7 +26,7 @@ export class TasksController {
   // }
 
   @Get()
-  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+  getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
     if (Object.keys(filterDto).length) {
       return this.tasksService.getTasksWithFilter(filterDto);
     } else {
