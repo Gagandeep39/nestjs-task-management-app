8d80bb43b08242ee7a2b21d74ed4e114345b8aad
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-06-18 18:17:39 +0530

    Moved insert logic to repository

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { TaskStatus } from './task-status.enum';
 import { Task } from './task.entity';
 import { TaskRepository } from './task.repository';
 
@@ -9,18 +8,7 @@ export class TasksService {
   constructor(private tasksRepository: TaskRepository) {}
 
   async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
-    // Create task
-    const { title, description } = createTaskDto;
-    const task = new Task();
-    task.title = title;
-    task.description = description;
-    task.status = TaskStatus.OPEN;
-
-    // Save to DB
-    await task.save();
-
-    // Return new task
-    return task;
+    return this.tasksRepository.createTask(createTaskDto);
   }
 
   async getTaskById(id: number): Promise<Task> {
