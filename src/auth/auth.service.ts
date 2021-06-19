6e6b686134d8ef7a8a0c1dda198afa427a1a90c9
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-06-19 10:19:30 +0530

    Sign in method now sends jwt token in response

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,23 +1,32 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { JwtPayload } from './jwt-payload.model';
 import { UserRepository } from './user.repository';
 
 @Injectable()
 export class AuthService {
-  constructor(private userRepository: UserRepository) {}
+  constructor(
+    private userRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.signUp(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const username = await this.userRepository.validateUserPassword(
       authCredentialsDto,
     );
     if (!username) {
       throw new UnauthorizedException('Invalid Credentials');
-    } else {
-      return username;
     }
+
+    const payload: JwtPayload = { username };
+    const accessToken = this.jwtService.sign(payload);
+    return { accessToken };
   }
 }
