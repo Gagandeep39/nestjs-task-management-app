1cae8d59d26f3553a6241216baf2b8b71eae8e5c
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-06-20 13:55:20 +0530

    Added file based config values

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { get } from 'config';
+
+const dbConfig: any = get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'gagan',
-  password: '123456',
-  database: 'taskmanagement',
-  synchronize: true,
+  type: dbConfig.type,
+  host: dbConfig.host,
+  port: dbConfig.port,
+  username: dbConfig.username,
+  password: dbConfig.password,
+  database: dbConfig.database,
+  synchronize: dbConfig.synchronize,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'], // Any file with .entity.ts extension
 };
